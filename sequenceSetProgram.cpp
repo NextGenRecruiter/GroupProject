1850a3e414e04373a0eb12f8628437190e3f21d9
Jacob <59483327+smika6@users.noreply.github.com>
2020-04-27 15:39:40 -0500

    Rename and Started

---
+++ sequenceSetProgram.cpp
@@ -0,0 +1,34 @@
+/*
+  Authors: Jacob Hopkins
+  Date: 4/27/2020
+  Due: 5/1/2020
+  
+  TODO:
+    - Create SequenceSet Class in the 'SequenceSet.h' file
+    - Create a test driver for the class above
+    - Create the program for using the txt files with the class
+    - Create the design doccument for
+        - The Class
+        - The Test Driver
+    - A User Manual
+    - DyOxygen the Code
+*/
+
+
+//import header with the SequenceSet class
+#include "SequenceSet.h"
+#include <cstdio>
+
+/*
+  Here is the main function to start the program.
+*/
+int main(){
+  
+  SequenceSet test;
+  
+  test = new SequenceSet();
+  
+  
+  std::getchar();
+  return 0; 
+}
